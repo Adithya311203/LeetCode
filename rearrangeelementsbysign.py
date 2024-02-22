de09c0437f0bbfd5dfb225c6239b09f15633a651
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-02-23 01:24:14 +0530

    Create rearrangeelementsbysign.py

---
+++ rearrangeelementsbysign.py
@@ -0,0 +1,13 @@
+def rearrangeArray(self, nums: List[int]) -> List[int]:
+        l1=[]
+        l2=[]
+        l=[]
+        for i in nums:
+            if i<0:
+                l2.append(i)
+            else:
+                l1.append(i)
+        print(l1,l2)
+        for i in range(len(l2)):
+            l.extend([l1[i],l2[i]])
+        return l
