efa60b8a4b127b36cfc4bac520d09af513bf448f
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-02-24 23:48:25 +0530

    Update 349.py

--- 349.py
+++ 349.py
@@ -3,6 +3,7 @@ def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         s2=set(nums2)
         if len(s2)>len(s1):
             s2,s1=s1,s2
+
         l=[]
         for i in s1:
             if i in s2:
