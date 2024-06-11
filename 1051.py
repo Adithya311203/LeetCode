ce7e8d643ef7ad4237084ef16562a37d73223bec
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-06-11 23:37:25 +0530

    Update 1051.py

--- 1051.py
+++ 1051.py
@@ -1,7 +1,6 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         l=sorted(heights)
-        None
         c=0
         for i in range(len(l)):
             if l[i]!=heights[i]:
