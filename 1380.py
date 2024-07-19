d20e5f83577841ab6c9d1c39d11c3c8a668e5768
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-07-19 19:23:08 +0530

    Create 1380.py

---
+++ 1380.py
@@ -0,0 +1,20 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        d={}
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if j in d:
+                    d[j].append(matrix[i][j])
+                else:
+                    d[j]=list([matrix[i][j],])
+        x=[]
+        for i in matrix:
+            x.append(min(i))
+        y=[]
+        for i in list(d.values()):
+            y.append(max(i))
+        final=[]
+        for i in x:
+            if i in y:
+                final.append(i)
+        return final
